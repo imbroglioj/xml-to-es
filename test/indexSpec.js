60db33851220a1865427beba04acbd77695f99bf
John Broglio <jbroglio@chiliad.com>
2014-07-31 10:51:08 -0400

    skip index tests if no ES

--- test/indexSpec.js
+++ test/indexSpec.js
@@ -8,19 +8,49 @@
 var should = require('chai').should()
     , path = require('path')
     , core = require('../index.js')
-    , IndexSpecPage = require('./IndexSpecPage.js').IndexSpecPage
+    , request = require('superagent')
     ;
+var IndexSpecPage, page;
 
-describe("sgml tests", function () {
-    var page= new IndexSpecPage(core, path, should);
+describe("Index tests (results not valid if ES not running)", function () {
     core.logger.info("indexSpec: "+new Date());
-    it('should index single file', function(done){
-        page.testIndexSingleObject(done);
+    var noES;
+
+    // have to call this every time since async
+    function checkES(cb){
+        if (noES) return cb("ES not running.");
+        request
+            .get('http://localhost:9200/')
+            .end(function(err, res){
+                if (err || !res.ok) {
+                    noES = true;
+                    core.logger.warn("*** Will skip index test because ElasticSearch not running at localhost:9200 ***");
+                    return cb(err);
+                }
+                cb();
+            });
+    }
+    // is there a dynamic way to get mocha to skip tests?
+    checkES(function(err){
+        if (!err) {
+            IndexSpecPage = require('./IndexSpecPage.js').IndexSpecPage,
+                page = new IndexSpecPage(core, path, should);
+        }
     });
 
-    it('should index json file aggr', function(done){
-        this.timeout(2000);
-       page.testIndexAggregateFile(done);
+    it('should index single file', function(done) {
+        checkES(function (err) {
+            if (err) return done();
+            page.testIndexSingleObject(done);
+        });
+    });
+
+    it('should index json file aggr', function(done) {
+        checkES(function (err) {
+            if (err) return done();
+            this.timeout(2000);
+            page.testIndexAggregateFile(done);
+        });
     });
 
 });
