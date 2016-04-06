412ab42c9060b21afe91d98e357c7eeada450785
jbroglio <jbroglio@gmail.com>
2016-04-06 14:30:34 -0400

    passing all tests with chunk by chunk read

--- test/IndexSpecPage.js
+++ test/IndexSpecPage.js
@@ -6,98 +6,108 @@
  */
 
 var FileSpecPage = require('./FileSpecPage.js').FileSpecPage
-    , util = require('util')
-    , child = require('child_process')
-    , fs = require('fs')
-    ;
+  , util = require('util')
+  , child = require('child_process')
+  , fs = require('fs')
+  ;
 
 exports.IndexSpecPage = function (core, path, should) {
-    var self = this;
-    var filePage = new FileSpecPage(core, path, should);
-    var argv = {
-        _: ['N/A'],
-        config: path.resolve(__dirname, '../examples/index-es-config.js'),
-        level: 'error',
+  var self = this;
+  var filePage = new FileSpecPage(core, path, should);
+  var argv = {
+    _: ['N/A'],
+    config: path.resolve(__dirname, '../examples/index-es-config.js'),
+    level: 'error',
+    clean: true
+  };
+  var argvInline = {
+    _: ['N/A'],
+    config: {
+      index: {
+        name: 'testxml__',
+        type: 'doc',  // must match one of the types in mapping
+        settings: {index: {number_of_shards: 1}},
+        mapping: path.join(__dirname, '../examples/mapping.json'),
+        ext: '.json',
+        server: 'localhost',
+        port: 9200,
         clean: true
-    };
-    var argvInline = {
-        _: ['N/A'],
-        config: {
-            index: {
-                name: 'testxml__',
-                type: 'doc',  // must match one of the types in mapping
-                settings: {index: {number_of_shards: 1}},
-                mapping: path.join(__dirname, '../examples/mapping.json'),
-                ext: '.json',
-                server: 'localhost',
-                port: 9200,
-                clean: true
-            }
-        },
-        level: 'error'
-    };
-    self.testIndexSingleObject = function (done) {
-        var config = filePage.makeJsonInlineConfig(filePage.simpleFile, argvInline.config);
-        config.generator = function (json, cb) {
-            if (filePage.jsonDone(json))
-                return cb ? cb() : done ? done() : null;
-            config.indexer.putMapping(function (err) {
-                should.not.exist(err);
-                config.indexer.submitObject(json, 'N/A', function (err) {
-                    should.not.exist(err);
-                    // callback since only one file
-                    config.indexer.deleteIndex(cb || done);
-                });
-            });
-        };
-        // get updated config
-        config = config.indexer.getConfig();
-        config.indexer.deleteIndex(config.indexer.indexTypeUrl, function (err) {
-            if (err) return true.should.be.false();
-
-            new core.Parser(config).processFiles(done);
+      }
+    },
+    level: 'error'
+  };
+  self.testIndexSingleObject = function (done) {
+    var config = filePage.makeJsonInlineConfig(filePage.simpleFile, argvInline.config);
+    config.generator = function (json, cb) {
+      if (filePage.jsonDone(json))
+        return cb ? cb() : done ? done() : null;
+      config.indexer.putMapping(function (err) {
+        should.not.exist(err);
+        config.indexer.submitObject(json, 'N/A', function (err) {
+          should.not.exist(err);
+          // callback since only one file
+          config.indexer.deleteIndex(cb || done);
         });
+      });
     };
+    // get updated config
+    config = config.indexer.getConfig();
+    config.indexer.deleteIndex(config.indexer.indexTypeUrl, function (err) {
+      if (err) return true.should.be.false();
 
-    // question should we just test file here
-    self.testIndexAggregateFile = function (done) {
-        // use child procs and call on files
-        var indexDir = path.resolve(process.cwd(), 'jsonTest');
-        if (fs.existsSync(indexDir)) {
-            fs.readdirSync(indexDir, function (f) {
-                fs.unlinkSync(path.join(indexDir, f));
-            });
-        } else fs.mkdirSync(indexDir);
-        var config = filePage.makeJsonConfig(filePage.goodTags,
-             {input:{fileExt:'.sgm'}, output: {destDir: indexDir, docsPerFile: 0}});
-        core.resolveIndexOptions({
-                _: [indexDir],
-            level: "WARN",
-                config: argv.config,
-                clean: true
-            });
-        // get updated config
-        config = config.indexer.getConfig();
-        config.indexer.deleteIndex(config.indexer.indexTypeUrl, function (err) {
-            if (err) return true.should.be.false();
+      new core.Parser(config).processFiles(done);
+    });
+  };
 
-            new core.Parser(config).processFiles(function (err) {
-                if (err) return done ? done() : null;
-                config.indexer.putMapping(function (e) {
-                    should.not.exist(e);
-                    // give time to make sure parser write is done. todo: making writestream async is tough, but try it.
-                    config.indexer.putFiles([indexDir], function (e1) {
-                        should.not.exist(e1);
-                        config.indexer.getDocumentCount(function (e2, json) {
-                            should.not.exist(e2);
-                            should.exist(json);
-                            should.exist(json.count);
-                            json.count.should.be.greaterThan(0);
-                            config.indexer.deleteIndex(done);
-                        });
-                    });
-                });
+  // question should we just test file here
+  self.testIndexAggregateFile = function (done) {
+    // use child procs and call on files
+    var indexDir = path.resolve(process.cwd(), 'jsonTest');
+
+    if (fs.existsSync(indexDir)) {
+      fs.readdir(indexDir, function (err, files) {
+        if (files && files.length) files.forEach(function(f){
+          fs.unlinkSync(path.join(indexDir, f));
+        })
+        doFile();
+      });
+    } else {
+      fs.mkdirSync(indexDir);
+      doFile();
+    }
+
+    function doFile() {
+      var config = filePage.makeJsonConfig(filePage.goodTags,
+        {input: {fileExt: '.sgm'}, output: {destDir: indexDir, docsPerFile: 0}});
+      core.resolveIndexOptions({
+        _: [indexDir],
+        level: "WARN",
+        config: argv.config,
+        clean: true
+      });
+      // get updated config
+      config = config.indexer.getConfig();
+      config.indexer.deleteIndex(config.indexer.indexTypeUrl, function (err) {
+        if (err) return true.should.be.false();
+
+        new core.Parser(config).processFiles(function (err) {
+          if (err) return done ? done() : null;
+          config.indexer.putMapping(function (e) {
+            should.not.exist(e);
+            // give time to make sure parser write is done. todo: making writestream async is tough, but try it.
+            config.indexer.putFiles([indexDir], function (e1) {
+              should.not.exist(e1);
+              config.indexer.getDocumentCount(function (e2, json) {
+                should.not.exist(e2);
+                should.exist(json);
+                should.exist(json.count);
+                json.count.should.be.greaterThan(0);
+                config.indexer.deleteIndex(done);
+              });
             });
+          });
         });
-    };
+      });
+    }
+  };
 };
